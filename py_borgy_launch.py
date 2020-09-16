c13fc3ac19273521460bb1391dd3c209e8646eba
Massimo Caccia <optimass@dc1-wks-09.elementai.net>
2020-09-16 13:13:30 +0000

    doing a random commit cause I fucked up

--- py_borgy_launch.py
+++ py_borgy_launch.py
@@ -6,7 +6,7 @@ import random
 def id_generator(size=8, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
-debug = 0
+debug = 1
 
 wandb = 'pupper4'
 
